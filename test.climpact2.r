8d8ec2815ddae9ef78968f3083aec3bca9224a84
heroldn <herold.n@gmail.com>
2015-04-01 09:59:49 +1100

    can write out and read percentiles to/from a file, also reduced percentiles to calculate for each index to improve performance. Improved time dimension output.

--- test.climpact2.r
+++ test.climpact2.r
@@ -1,10 +1,13 @@
 # Testing template for climpact.loader
-source("climpact2_0.2.r")
+source("climpact2_0.3.r")
 
-indices=c("tm5a","tx95t","rxnday")
-minfilename = "/srv/ccrc/data43/z3506872/datasets/sample_narclim/CCRC_NARCliM_DAY_1950-2009_tasmin.nc"
-maxfilename = "/srv/ccrc/data43/z3506872/datasets/sample_narclim/CCRC_NARCliM_DAY_1950-2009_tasmax.nc"
-precfilename = "/srv/ccrc/data43/z3506872/datasets/sample_narclim/CCRC_NARCliM_DAY_1950-2009_pracc_fl.nc"
+indices=c("tx95t")
+minfilename = "/srv/ccrc/data43/z3506872/datasets/sample_narclim/CCRC_NARCliM_DAY_1950-2009_tasmin.reduced.nc"
+maxfilename = "/srv/ccrc/data43/z3506872/datasets/sample_narclim/CCRC_NARCliM_DAY_1950-2009_tasmax.reduced.nc"
+precfilename = "/srv/ccrc/data43/z3506872/datasets/sample_narclim/CCRC_NARCliM_DAY_1950-2009_pracc_fl.reduced.nc"
+
+#startss()
+#q()
 
 climpact.loader(
 precfile=precfilename,
@@ -13,10 +16,15 @@ tsmaxfile=maxfilename,
 precname="pracc_fl",
 tsminname="tasmin",
 tsmaxname="tasmax",
-tempqtiles=c(0.1,0.5,0.9,0.95),
-ntxntn_consecdays=8,
+tempqtiles=c(0.1,0.6,0.9,0.95),
+precqtiles=c(0.1,0.9,0.95,0.99),
+write_quantiles=FALSE,
+#quantile_file = "CCRC_NARCliM-test_DAY_1961_1990_quantiles.nc",
+ntxntn_consecdays=7,
+ntxbntnb_consecdays=7,
 rxnday_n=7,
+wsdin_n=7,
 indices=indices,
 baserange=c(1961,1990),
 #time_format="%Y%m%d",
-identifier="NARCliM_test")
+identifier="NARCliM-test")
